f20d5ccee58bf22e5394445c3bbef901c3eda5da
RAKIB6348 <shahidulhuda0@gmail.com>
2025-11-18 21:15:21 +0600

    create class list page and urls

--- academics/urls.py
+++ academics/urls.py
@@ -4,4 +4,5 @@ from academics.views import *
 
 urlpatterns = [
     path('add-class/', add_class, name='add_class'),
+    path('class-list/', class_list, name='class_list'),
 ]
--- academics/views.py
+++ academics/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#=============== add class =============================
 def add_class(request):
 
-    return render(request, 'class_acadmi/add_class.html')
+    return render(request, 'academic/class/register.html')
+
+
+
+#=============== list class ======================
+def class_list(request):
+
+    return render(request, 'academic/class/class_list.html')
