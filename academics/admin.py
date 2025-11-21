b48331247274cc84f93cdf571a38ceaefe71bb2b
RAKIB6348 <shahidulhuda0@gmail.com>
2025-11-21 22:03:00 +0600

    create school class model

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from academics.models import SchoolClass
 
 # Register your models here.
+class SchoolClassAdmin(admin.ModelAdmin):
+
+    list_display = ['class_name', 'class_code', 'created_at']
+
+    search_fields = ['class_name', 'class_code']
+
+admin.site.register(SchoolClass, SchoolClassAdmin)
--- academics/models.py
+++ academics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SchoolClass(models.Model):
+    class_name = models.CharField(max_length=100)
+    class_code = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.class_name}"
